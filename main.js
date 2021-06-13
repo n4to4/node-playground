e4a37b8738f91234a767cf3d6372138e06182633
n4to4 <n4to4k@gmail.com>
2021-06-13 19:59:07 +0900

    snapshot

--- main.js
+++ main.js
@@ -2,32 +2,32 @@
 
 const { spawnSync } = require('child_process');
 const rl = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 function question(query) {
-	return new Promise(resolve => {
-		rl.question(query, resolve);
-	});
+  return new Promise(resolve => {
+    rl.question(query, resolve);
+  });
 }
 
 async function main() {
-	console.log('started');
+  console.log('started');
 
-	const result1 = spawnSync('sleep', ['1']);
-	console.log(result1);
+  const result1 = spawnSync('sleep', ['1']);
+  console.log(result1);
 
-	const answer1 = await question('q? ');
-	console.log(answer1);
+  const answer1 = await question('q? ');
+  console.log(answer1);
 
-	const result2 = spawnSync('time');
-	console.log(result2);
+  const result2 = spawnSync('time');
+  console.log(result2);
 
-	const answer2 = await question('q? ');
-	console.log(answer2);
+  const answer2 = await question('q? ');
+  console.log(answer2);
 
-	rl.close();
+  rl.close();
 }
 
 main();
